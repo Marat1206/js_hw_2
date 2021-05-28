bdaa024f8e2d4b32ff0af9a101781f551fa0e2fe
Marat1206 <marikzhekov@gmail.com>
2021-05-28 17:51:55 +0300

    hm_2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let name = 'Джон';
+let admin = name;
+alert(admin);
+
+// let ourPlanet = 'Земля';
+// let currentUserName = 'Марат';
+
+// let num = '10' + 10;
+// console.log(num,  typeof(num));
+// Операмба - '10' является строчной, за счет этого оператор (+) производит конкотенацию и мы получаем 1010 с typeof - (string)
