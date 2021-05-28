56f063a21999f456a64e3ac2d703a79f6e0763b1
Marat1206 <marikzhekov@gmail.com>
2021-05-28 20:26:32 +0300

    hw_2_add.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let name = 'Джон';
-let admin = name;
-alert(admin);
+// let name = 'Джон';
+// let admin = name;
+// alert(admin);
 
 // let ourPlanet = 'Земля';
 // let currentUserName = 'Марат';
@@ -8,3 +8,26 @@ alert(admin);
 // let num = '10' + 10;
 // console.log(num,  typeof(num));
 // Операмба - '10' является строчной, за счет этого оператор (+) производит конкотенацию и мы получаем 1010 с typeof - (string)
+
+// -----------------------------------доп---------------------------------------------------------------
+//
+// let name = 'John'
+//
+// let user = {
+//     name: 'Pete',
+//     sureName: 'Smith',
+// }
+// console.log(user)
+//
+// delete user.name;
+// console.log(user)
+
+
+const user = {
+    name: 'john',
+    sureName: 'Smith'
+}
+console.log(user)
+
+user.name = 'Pete'
+console.log(user);
